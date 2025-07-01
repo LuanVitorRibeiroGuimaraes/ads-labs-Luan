7104c38cceff9287338f8cf6daff0adc97b9594b
Luan <luanvitor.mat@gmail.com>
2025-07-01 11:07:43 -0300

    fix: erro de conexão com postgres

--- semana 1/Node/database.js
+++ semana 1/Node/database.js
@@ -2,11 +2,10 @@ require('dotenv').config({ path: '.env' });
 
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(process.env.CONECTION_URI, {
   dialect: 'postgres',
   logging: false // opcional, só pra limpar o terminal
 });
-//'postgresql://postgres:1649312@localhost:5432/Restaurante-API'
 
 
 
